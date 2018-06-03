24283419e326d5133ff91496531719fca26f1b78
WenbinZhu <zwbhsefz@gmail.com>
2018-06-03 02:54:50 -0700

    Finish metadata store centralized version, untested

--- surfstore/java/src/main/java/surfstore/MetadataStore.java
+++ surfstore/java/src/main/java/surfstore/MetadataStore.java
@@ -2,9 +2,15 @@ package surfstore;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
@@ -12,7 +18,12 @@ import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
+import surfstore.SurfStoreBasic.Block;
 import surfstore.SurfStoreBasic.Empty;
+import surfstore.SurfStoreBasic.FileInfo;
+import surfstore.SurfStoreBasic.SimpleAnswer;
+import surfstore.SurfStoreBasic.WriteResult;
+
 
 public final class MetadataStore {
     private static final Logger logger = Logger.getLogger(MetadataStore.class.getName());
@@ -26,7 +37,7 @@ public final class MetadataStore {
 
 	private void start(int port, int numThreads) throws IOException {
         server = ServerBuilder.forPort(port)
-                .addService(new MetadataStoreImpl())
+                .addService(new MetadataStoreImpl(config))
                 .executor(Executors.newFixedThreadPool(numThreads))
                 .build()
                 .start();
@@ -91,6 +102,23 @@ public final class MetadataStore {
     }
 
     static class MetadataStoreImpl extends MetadataStoreGrpc.MetadataStoreImplBase {
+        private boolean isLeader;
+        private boolean crashed;
+        private Map<String, Integer> versionMap;
+        private Map<String, List<String>> blockListMap;
+
+        private BlockStoreGrpc.BlockStoreBlockingStub blockStub;
+
+        MetadataStoreImpl(ConfigReader config) {
+            this.isLeader = true;
+            this.versionMap = new HashMap<>();
+            this.blockListMap = new HashMap<>();
+
+            ManagedChannel channel = ManagedChannelBuilder.forAddress("127.0.0.1",
+                    config.getBlockPort()).usePlaintext(true).build();
+            this.blockStub = BlockStoreGrpc.newBlockingStub(channel);
+        }
+
         @Override
         public void ping(Empty req, final StreamObserver<Empty> responseObserver) {
             Empty response = Empty.newBuilder().build();
@@ -98,6 +126,156 @@ public final class MetadataStore {
             responseObserver.onCompleted();
         }
 
-        // TODO: Implement the other RPCs!
+        @Override
+        public void readFile(SurfStoreBasic.FileInfo request, StreamObserver<SurfStoreBasic.FileInfo> responseObserver) {
+            FileInfo.Builder builder = FileInfo.newBuilder();
+            builder.setFilename(request.getFilename());
+
+            synchronized (this) {
+                Integer version = versionMap.get(request.getFilename());
+                List<String> blockList = blockListMap.get(request.getFilename());
+
+                if (version != null) {
+                    builder.setVersion(version);
+                }
+                if (blockList != null) {
+                    builder.addAllBlocklist(blockList);
+                }
+            }
+
+            FileInfo response = builder.build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public void modifyFile(SurfStoreBasic.FileInfo request, StreamObserver<SurfStoreBasic.WriteResult> responseObserver) {
+            WriteResult.Builder builder = WriteResult.newBuilder();
+            String filename = request.getFilename();
+            int cVersion = request.getVersion();
+            int sVersion;
+
+            if (!isLeader) {
+                builder.setResult(WriteResult.Result.NOT_LEADER);
+                responseObserver.onNext(builder.build());
+                responseObserver.onCompleted();
+                return;
+            }
+
+            synchronized (this) {
+                sVersion = versionMap.getOrDefault(filename, 0);
+                if (cVersion != sVersion + 1) {
+                    builder.setResult(WriteResult.Result.OLD_VERSION).setCurrentVersion(sVersion);
+                    responseObserver.onNext(builder.build());
+                    responseObserver.onCompleted();
+                    return;
+                }
+
+                List<String> missingBlocks = new ArrayList<>();
+                for (String hash : request.getBlocklistList()) {
+                    Block block = SurfStoreBasic.Block.newBuilder().setHash(hash).build();
+                    if (!blockStub.hasBlock(block).getAnswer()) {
+                        missingBlocks.add(hash);
+                    }
+                }
+
+                if (!missingBlocks.isEmpty()) {
+                    builder.setResult(WriteResult.Result.MISSING_BLOCKS).setCurrentVersion(sVersion)
+                            .addAllMissingBlocks(missingBlocks);
+                    responseObserver.onNext(builder.build());
+                    responseObserver.onCompleted();
+                    return;
+                }
+
+                versionMap.put(filename, cVersion);
+                blockListMap.put(filename, request.getBlocklistList());
+
+                builder.setResult(WriteResult.Result.OK).setCurrentVersion(cVersion);
+                responseObserver.onNext(builder.build());
+                responseObserver.onCompleted();
+            }
+        }
+
+        @Override
+        public void deleteFile(SurfStoreBasic.FileInfo request, StreamObserver<SurfStoreBasic.WriteResult> responseObserver) {
+            WriteResult.Builder builder = WriteResult.newBuilder();
+            String filename = request.getFilename();
+            int cVersion = request.getVersion();
+            int sVersion;
+
+            if (!isLeader) {
+                builder.setResult(WriteResult.Result.NOT_LEADER);
+                responseObserver.onNext(builder.build());
+                responseObserver.onCompleted();
+                return;
+            }
+
+            synchronized (this) {
+                sVersion = versionMap.getOrDefault(filename, 0);
+                if (sVersion == 0 || cVersion != sVersion + 1) {
+                    builder.setResult(WriteResult.Result.OLD_VERSION).setCurrentVersion(sVersion);
+                    responseObserver.onNext(builder.build());
+                    responseObserver.onCompleted();
+                    return;
+                }
+
+                versionMap.put(filename, cVersion);
+                List<String> singleHashList = new ArrayList<>();
+                singleHashList.add("0");
+                blockListMap.put(filename, singleHashList);
+
+                builder.setResult(WriteResult.Result.OK).setCurrentVersion(cVersion);
+                responseObserver.onNext(builder.build());
+                responseObserver.onCompleted();
+            }
+        }
+
+        @Override
+        public void isLeader(Empty request, StreamObserver<SurfStoreBasic.SimpleAnswer> responseObserver) {
+            SimpleAnswer response = SimpleAnswer.newBuilder().setAnswer(isLeader).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public void crash(Empty request, StreamObserver<Empty> responseObserver) {
+            crashed = true;
+            Empty response = Empty.newBuilder().build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public void restore(Empty request, StreamObserver<Empty> responseObserver) {
+            crashed = false;
+            Empty response = Empty.newBuilder().build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public void isCrashed(Empty request, StreamObserver<SurfStoreBasic.SimpleAnswer> responseObserver) {
+            SimpleAnswer response = SimpleAnswer.newBuilder().setAnswer(crashed).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public void getVersion(SurfStoreBasic.FileInfo request, StreamObserver<SurfStoreBasic.FileInfo> responseObserver) {
+            FileInfo.Builder builder = FileInfo.newBuilder();
+            builder.setFilename(request.getFilename());
+
+            synchronized (this) {
+                Integer version = versionMap.get(request.getFilename());
+
+                if (version != null) {
+                    builder.setVersion(version);
+                }
+            }
+
+            FileInfo response = builder.build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
     }
 }
