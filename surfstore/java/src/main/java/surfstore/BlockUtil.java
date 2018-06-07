579eaa6a8ae311583f58be0a5392685cf5a0acef
WenbinZhu <zwbhsefz@gmail.com>
2018-06-07 02:05:08 -0700

    Add client implementation, lightly tested

--- surfstore/java/src/main/java/surfstore/BlockUtil.java
+++ surfstore/java/src/main/java/surfstore/BlockUtil.java
@@ -1,13 +1,17 @@
 package surfstore;
 
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
+import java.util.*;
 
 public class BlockUtil {
 
-    public static String sha256(String s) {
+    private static final int BLOCKSIZE = 4 * 1024;
+
+    static String sha256(String s) {
         MessageDigest digest;
 
         try {
@@ -21,6 +25,75 @@ public class BlockUtil {
         String encoded = Base64.getEncoder().encodeToString(hash);
 
         return encoded;
+    }
+
+    static List<String> readBlocksFromFile(String filepath) {
+        List<String> blocks = new ArrayList<>();
+
+        try (FileInputStream fis = new FileInputStream(filepath)) {
+            byte[] buf = new byte[BLOCKSIZE];
+            int size;
+
+            while ((size = fis.read(buf)) != -1) {
+                blocks.add(new String(buf, 0, size, StandardCharsets.UTF_8));
+                buf = new byte[BLOCKSIZE];
+            }
+        } catch (FileNotFoundException e) {
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+           return null;
+        }
+
+        return blocks;
+    }
+
+    static Map<String, byte[]> scanBlocksInDir(String dir) {
+        File dirFile = new File(dir);
+        File[] files = dirFile.listFiles(File::isFile);
+
+        if (files == null) {
+            return null;
+        }
+
+        Map<String, byte[]> hashBlockMap = new HashMap<>();
+        for (File file : files) {
+            try (FileInputStream fis = new FileInputStream(file)) {
+                byte[] buf = new byte[BLOCKSIZE];
+                int size;
+
+                while ((size = fis.read(buf)) != -1) {
+                    String block = new String(buf, 0, size, StandardCharsets.UTF_8);
+                    buf = size < BLOCKSIZE ? Arrays.copyOfRange(buf, 0, size) : buf;
+                    hashBlockMap.put(sha256(block), buf);
+                    buf = new byte[BLOCKSIZE];
+                }
+            } catch (FileNotFoundException e) {
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        return hashBlockMap;
+    }
+
+    static boolean writeBlocksToFile(String filename, String dir, List<byte[]> blocks) {
+        File writeFile = Paths.get(dir, filename).toFile();
+
+        try (FileOutputStream fos = new FileOutputStream(writeFile)) {
+            for (byte[] block : blocks) {
+                fos.write(block);
+            }
+        } catch (FileNotFoundException e) {
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
--- surfstore/java/src/main/java/surfstore/Client.java
+++ surfstore/java/src/main/java/surfstore/Client.java
@@ -2,6 +2,10 @@ package surfstore;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
@@ -14,14 +18,15 @@ import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 
 import surfstore.SurfStoreBasic.Block;
-import surfstore.SurfStoreBasic.Empty;
+import surfstore.SurfStoreBasic.FileInfo;
+import surfstore.SurfStoreBasic.WriteResult;
 
 
 public final class Client {
     private static final Logger logger = Logger.getLogger(Client.class.getName());
 
-    private final ManagedChannel metadataChannel;
-    private final MetadataStoreGrpc.MetadataStoreBlockingStub metadataStub;
+    private final ManagedChannel leaderChannel;
+    private final MetadataStoreGrpc.MetadataStoreBlockingStub leaderStub;
 
     private final ManagedChannel blockChannel;
     private final BlockStoreGrpc.BlockStoreBlockingStub blockStub;
@@ -29,9 +34,10 @@ public final class Client {
     private final ConfigReader config;
 
     public Client(ConfigReader config) {
-        this.metadataChannel = ManagedChannelBuilder.forAddress("127.0.0.1", config.getMetadataPort(1))
+        int leader = config.getLeaderNum();
+        this.leaderChannel = ManagedChannelBuilder.forAddress("127.0.0.1", config.getMetadataPort(leader))
                 .usePlaintext(true).build();
-        this.metadataStub = MetadataStoreGrpc.newBlockingStub(metadataChannel);
+        this.leaderStub = MetadataStoreGrpc.newBlockingStub(leaderChannel);
 
         this.blockChannel = ManagedChannelBuilder.forAddress("127.0.0.1", config.getBlockPort())
                 .usePlaintext(true).build();
@@ -41,7 +47,7 @@ public final class Client {
     }
 
     public void shutdown() throws InterruptedException {
-        metadataChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+        leaderChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
         blockChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
 
@@ -53,35 +59,144 @@ public final class Client {
 
         return builder.build();
     }
-    
-	private void go() {
-        // metadataStub.ping(Empty.newBuilder().build());
-        // logger.info("Successfully pinged the Metadata server");
-        
-        blockStub.ping(Empty.newBuilder().build());
-        logger.info("Successfully pinged the Blockstore server");
 
-        Block b1 = stringToBlock("block_1");
-        Block b2 = stringToBlock("block_2");
+	private void go(Namespace c_args) {
+        String command = c_args.getString("command");
+        String filename = c_args.getString("target_file");
+        String directory = c_args.getString("download_dir");
 
-        System.out.println(blockStub.hasBlock(b1).getAnswer());
-        System.out.println(blockStub.hasBlock(b2).getAnswer());
-
-        blockStub.storeBlock(b1);
-        blockStub.storeBlock(b2);
-
-        System.out.println(blockStub.hasBlock(b1).getAnswer());
-        System.out.println(blockStub.hasBlock(b2).getAnswer());
+        if (command.equals("upload")) {
+            upload(filename);
+        } else if (command.equals("download")) {
+            download(filename, directory);
+        } else if (command.equals("delete")) {
+            delete(filename);
+        } else if (command.equals("getversion")) {
+            getVersion(filename);
+        } else {
+            throw new IllegalArgumentException("command not supported");
+        }
 	}
 
-	/*
-	 * TODO: Add command line handling here
-	 */
+	private synchronized void upload(String filepath) {
+        String[] path = filepath.trim().split("/");
+        String filename = path[path.length - 1];
+        FileInfo.Builder builder = FileInfo.newBuilder().setFilename(filename);
+
+        List<String> dataBlocks = BlockUtil.readBlocksFromFile(filepath);
+        if (dataBlocks == null) {
+            System.out.println("Not Found");
+            return;
+        }
+
+        int version = leaderStub.getVersion(builder.build()).getVersion();
+        builder.setVersion(++version);
+
+        Map<String, Block> blockMap = new HashMap<>();
+        for (String d : dataBlocks) {
+            Block block = stringToBlock(d);
+            builder.addBlocklist(block.getHash());
+            blockMap.put(block.getHash(), block);
+        }
+
+        while (true) {
+            WriteResult response = leaderStub.modifyFile(builder.build());
+            if (response.getResult() == WriteResult.Result.NOT_LEADER) {
+                System.out.println("Not Leader");
+                return;
+            }
+            while (response.getResult() == WriteResult.Result.OLD_VERSION) {
+                builder.setVersion(++version);
+                response = leaderStub.modifyFile(builder.build());
+            }
+            if (response.getResult() == WriteResult.Result.MISSING_BLOCKS) {
+                for (String hash : response.getMissingBlocksList()) {
+                    blockStub.storeBlock(blockMap.get(hash));
+                }
+            }
+            if (response.getResult() == WriteResult.Result.OK) {
+                System.out.println("OK");
+                return;
+            }
+        }
+    }
+
+    private synchronized void download(String filename, String dir) {
+        FileInfo request = FileInfo.newBuilder().setFilename(filename).build();
+        FileInfo response = leaderStub.readFile(request);
+
+        Map<String, byte[]> hashBlockMap = BlockUtil.scanBlocksInDir(dir);
+        if (hashBlockMap == null) {
+            System.out.println("Not Found");
+            return;
+        }
+
+        if (response.getVersion() == 0 || response.getBlocklistList().isEmpty()) {
+            System.out.println("Not Found");
+            return;
+        }
+
+        List<byte[]> blocksToWrite = new ArrayList<>();
+        for (String hash : response.getBlocklistList()) {
+            if (hashBlockMap.containsKey(hash)) {
+                blocksToWrite.add(hashBlockMap.get(hash));
+            } else {
+                Block block = blockStub.getBlock(Block.newBuilder().setHash(hash).build());
+                blocksToWrite.add(block.getData().toByteArray());
+            }
+        }
+
+        if (BlockUtil.writeBlocksToFile(filename, dir, blocksToWrite)) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Not Found");
+        }
+    }
+
+    private synchronized void delete(String filename) {
+        FileInfo.Builder builder = FileInfo.newBuilder().setFilename(filename);
+        int version = leaderStub.getVersion(builder.build()).getVersion();
+
+        if (version == 0) {
+            System.out.println("Not Found");
+            return;
+        }
+
+        builder.setVersion(++version);
+        WriteResult response = leaderStub.deleteFile(builder.build());
+
+        if (response.getResult() == WriteResult.Result.NOT_LEADER) {
+            System.out.println("Not Leader");
+            return;
+        }
+        while (response.getResult() == WriteResult.Result.OLD_VERSION) {
+            builder.setVersion(++version);
+            response = leaderStub.deleteFile(builder.build());
+        }
+
+        System.out.println("OK");
+    }
+
+    private synchronized void getVersion(String filename) {
+        FileInfo request = FileInfo.newBuilder().setFilename(filename).build();
+        System.out.println(leaderStub.getVersion(request).getVersion());
+    }
+
     private static Namespace parseArgs(String[] args) {
         ArgumentParser parser = ArgumentParsers.newFor("Client").build()
                 .description("Client for SurfStore");
         parser.addArgument("config_file").type(String.class)
                 .help("Path to configuration file");
+        parser.addArgument("command").type(String.class)
+                .choices("upload", "download", "delete", "getversion")
+                .help("Client command, upload/download/delete/getversion");
+        parser.addArgument("target_file").type(String.class)
+                .help("Path of file to upload or filename to download/delete/getversion");
+
+        if (args[1].equals("download")) {
+            parser.addArgument("download_dir").type(String.class)
+                    .help("Path of download directory");
+        }
         
         Namespace res = null;
         try {
@@ -89,6 +204,7 @@ public final class Client {
         } catch (ArgumentParserException e) {
             parser.handleError(e);
         }
+
         return res;
     }
 
@@ -104,10 +220,9 @@ public final class Client {
         Client client = new Client(config);
         
         try {
-        	client.go();
+        	client.go(c_args);
         } finally {
             client.shutdown();
         }
     }
-
 }
